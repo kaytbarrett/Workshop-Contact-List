00ed74861679a8295a2b7b3aab7d6d3971dc3095
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-09-14 13:27:59 -0600

    table created

---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ContactRow ({ contact }) {
+return (
+<tr>
+<td>{contact.name}</td>
+<td>{contact.email}</td>
+<td>{contact.phone}</td>
+</tr>
+
+)
+}
+
+
+
+export default ContactRow;
